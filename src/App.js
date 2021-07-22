8ea1089237e0b308f52338d8eb6f07cc234344c1
joscrd <j194cc@gmail.com>
2021-07-22 14:34:29 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// Components
+import Home from './components/Home';
+import Pets from './components/Pets';
+import Upload from './components/Upload';
+import Contact from './components/Contact';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='navbar'>
+        <div>
+          <Link to="/">Inicio</Link>
+          <Link to="/pets">Mascotas</Link>
+          <Link to="/upload">Subir</Link>
+          <Link to="/contact">Contacto</Link>
+        </div>
+
+        <Switch>
+          <Route path="/upload">
+            <Upload />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route path="/pets">
+            <Pets />
+          </Route>
+
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Pets.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pets = () => {
+    return (  
+        <h2>pets</h2>
+    );
+}
+ 
+export default Pets;
